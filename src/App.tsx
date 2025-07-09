77ff65b52996e832443588f91d2baa1fd0162d92
Taylor180520 <158819422+Taylor180520@users.noreply.github.com>
2025-07-09 22:47:28 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -204,7 +204,7 @@ function App() {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
                 <input
                   type="text"
-                  placeholder="Search my knowledge vault..."
+                  placeholder="Search my knowledge books..."
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                   className="w-80 pl-10 pr-4 py-2 bg-gray-800/50 border border-gray-700 rounded-lg text-white placeholder-gray-500 focus:border-purple-600 focus:outline-none"
